d72e33699f5deea212ef9a3b5eeb0be85ce6bad4
mmr.K <pollvaulter.m@gmail.com>
2017-11-11 00:43:19 +0900

    make test code

--- src/main/java/alexa/Hello.java
+++ src/main/java/alexa/Hello.java
@@ -1,4 +1,13 @@
 package alexa;
 
-public class Hello {
+import alexa.model.Request;
+import alexa.model.Response;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Request, Response>{
+    public Response handleRequest(Request request, Context context) {
+        String greetingString = String.format("Hello %s %s.", request.getFirstName(), request.getLastName());
+        return new Response(greetingString);
+    }
 }
